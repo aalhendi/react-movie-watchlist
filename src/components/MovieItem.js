72c81ee914304454977bb8ae77f196c1a3153e2d
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-06-27 18:30:09 +0300

    Add delete button

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,15 +1,20 @@
 import movieStore from "../stores/movieStore";
 
 const MovieItem = (props) => {
-  const handleClick = () => {
+  const handleWatchButton = () => {
     movieStore.changeWatched(props.movie.id);
+  };
+
+  const handleDeleteButton = () => {
+    movieStore.deleteMovie(props.movie.id);
   };
   return (
     <div>
       <h1>{props.movie.name}</h1>
-      <button onClick={handleClick}>
+      <button onClick={handleWatchButton}>
         {props.movie.watched ? "Unwatch" : "Watch"}
       </button>
+      <button onClick={handleDeleteButton}>Delete</button>
     </div>
   );
 };
